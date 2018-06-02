af4196666f63dfe0357a0b93f52ee407a736b4b2
ines-vermeir <ines.vermeir@student.ehb.be>
2018-06-02 16:05:53 +0200

    project

---
+++ Ines_Vermeir_digx_werkstuk2/Ines_Vermeir_digx_werkstuk2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ines_Vermeir_digx_werkstuk2
+//
+//  Created by VERMEIR Inès (s) on 02/06/2018.
+//  Copyright © 2018 VERMEIR Inès (s). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Ines_Vermeir_digx_werkstuk2/Ines_Vermeir_digx_werkstuk2Tests/Ines_Vermeir_digx_werkstuk2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Ines_Vermeir_digx_werkstuk2Tests.swift
+//  Ines_Vermeir_digx_werkstuk2Tests
+//
+//  Created by VERMEIR Inès (s) on 02/06/2018.
+//  Copyright © 2018 VERMEIR Inès (s). All rights reserved.
+//
+
+import XCTest
+@testable import Ines_Vermeir_digx_werkstuk2
+
+class Ines_Vermeir_digx_werkstuk2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
